4a8bd5abd24330cc112d92314bc9613347fdec00
lerisand <70976808+lerisand@users.noreply.github.com>
2020-09-09 23:53:56 +0400

    Update Contents.swift

--- 1__SandLera.playdround.playground/Contents.swift
+++ 1__SandLera.playdround.playground/Contents.swift
@@ -7,44 +7,42 @@ let a = 34
 let b = 23
 let c = 19
 
-let Discriminant = sqrt((Double)(b*b)+(Double)(4*a*c))
-    if Discriminant == 0 {
-        let Radical = ((Double)(b)(Double)(-1))/((Double)(2)(Double)(a))
-//    не пойму в чем дело / пишет cannot call value of non-function type Double. буду искать решение после сдачи ДЗ
-        
-print("У уравнения (Equation) один корень и он равен (Radical)")
+let discriminant = sqrt Double(b*b)+ Double(4*a*c)
+    if discriminant == 0 {
+        let radical = Double(b)Double(-1)/Double(2)Double(a)    
+            print("У уравнения Equation один корень и он равен Radical")
 } else
-    if Discriminant < 0 {
-print("нет корней")
+    if discriminant < 0 {
+        print("нет корней")
 } else {
-        let RadicalOne = ((Double)(b)(Double)(-1) - Discriminant)/((Double)(2)(Double)(a))
-        let RadicalTwo = ((Double)(b)(Double)(-1) + Discriminant)/((Double)(2)(Double)(a))
-print("У уравнения (Equation) два корня (RadicalOne) и (RadicalTwo)")
+        let radicalOne = Double(b)Double(-1) - discriminant/Double(2)Double(a)
+        let radicalTwo = Double(b)Double(-1) + discriminant/Double(2)Double(a)
+            print("У уравнения Equation два корня radicalOne и radicalTwo")
 }
 
 
 
 // 2 задание
-let СathetA = 6
-let СathetB = 9
-let Area = (Double)(СathetA * СathetB) / 2.0
-let HypotenuseC = sqrt((Double)(СathetA * СathetA) + (Double)(СathetB * СathetB))
-let Perimeter = (Double)(СathetB + СathetA) + HypotenuseC
+let cathetA = 6
+let cathetB = 9
+let area = Double(cathetA * cathetB) / 2.0
+let hypotenuseC = sqrt(Double(cathetA * cathetA) + Double(cathetB * cathetB))
+let perimeter = Double(cathetB + cathetA) + hypotenuseC
 
-print("Площадь треугольника = (Area), периметр треугольника = (Perimeter), длина гипотенузы треугольника = (HypotenuseC)")
+print("Площадь треугольника = area, периметр треугольника = perimeter, длина гипотенузы треугольника = hypotenuseC")
 
 //3 задание
-typealias Ruble = Double
-typealias Percent = Double
-var AmountOfDeposit : Ruble = 2100.0
-var MeterYears = 1
-let AnnualPercentage : Percent = 5.0
-while MeterYears <= 5 {
-let OnePercent = AmountOfDeposit / 100.0
-let AnnualIncome = OnePercent * AnnualPercentage
-AmountOfDeposit = AmountOfDeposit + AnnualIncome
-MeterYears += 1
+typealias ruble = Double
+typealias percent = Double
+var amountOfDeposit : Ruble = 2100.0
+var meterYears = 1
+let annualPercentage : Percent = 5.0
+while meterYears <= 5 {
+let onePercent = amountOfDeposit / 100.0
+let annualIncome = onePercent * annualPercentage
+amountOfDeposit = amountOfDeposit + annualIncome
+meterYears += 1
 }
-print("Сумма вклада через 5 лет равна (AmountOfDeposit)")
+print("Сумма вклада через 5 лет равна amountOfDeposit")
 
 
